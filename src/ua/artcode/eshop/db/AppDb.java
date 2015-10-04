8d134e104f5ca7f6de3f108685481c5e6a603e61
presly808 <presly808@gmail.com>
2015-10-04 20:14:28 +0300

    week2 home

--- src/ua/artcode/eshop/db/AppDb.java
+++ src/ua/artcode/eshop/db/AppDb.java
@@ -3,8 +3,12 @@ package ua.artcode.eshop.db;
 import ua.artcode.eshop.exception.NoUserFoundException;
 import ua.artcode.eshop.model.Product;
 import ua.artcode.eshop.model.User;
+import ua.artcode.week1.hashstructure.MyHashMap;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by serhii on 24.09.15.
@@ -13,7 +17,7 @@ public class AppDb {
 
     private static int count = 0;
 
-    private Map<Integer, User> userMap;
+    private Map<Integer, User> userMap = new LinkedHashMap<>();
     private Map<Integer,Product> productMap;
 
 
--- src/ua/artcode/week1/tree/BTreeTestRun.java
+++ src/ua/artcode/week1/tree/BTreeTestRun.java
@@ -16,17 +16,27 @@ public class BTreeTestRun {
         BTree<Integer> bTree1 = new BST<>((Integer i1,Integer i2) -> {return i1 - i2;});
         BTree<String> bTree2 = new BST<>(String::compareTo);
 
-        bTree1.add(50);
-        bTree1.add(25);
-        bTree1.add(30);
-        bTree1.add(75);
+        bTree1.add(5);
+        bTree1.add(2);
+        bTree1.add(8);
+        bTree1.add(1);
+        bTree1.add(4);
+        bTree1.add(7);
+        bTree1.add(9);
+        bTree1.add(0);
+        bTree1.add(3);
+        bTree1.add(6);
+        bTree1.add(-5);
+        bTree1.add(15);
         bTree1.add(10);
-        bTree1.add(70);
-        bTree1.add(80);
+        bTree1.add(8);
 
 
+        BTree<Integer> bTree3 = new BST<>((Integer::compare), 100, 50,25,60,20,150,140,170,130,55, 30, 10,25);
+        System.out.println(bTree3.toString());
 
-        System.out.println(bTree1.toString());
+
+//        System.out.println(bTree1.toString());
 
 
 
